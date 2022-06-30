a79f0fea26e1b6a7b8841245f78fe2c878c2ee52
ploy-thanaporn <tnpsp1501@gmail.com>
2022-06-30 11:38:24 +0700

    create feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,16 @@
-import React from "react";
+import { SparklesIcon } from "@heroicons/react/outline";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
+      <div className="flex justify-between py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
+        <div className="hoverEffect flex items-center justify-center px-0 w-9 h-9">
+          <SparklesIcon className="h-5" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
