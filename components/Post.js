d746d972b7da0222d290b6d5244b6201a26fd2e3
ploy-thanaporn <tnpsp1501@gmail.com>
2022-07-01 14:04:06 +0700

    add the post section

---
+++ components/Post.js
@@ -0,0 +1,61 @@
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/outline";
+
+const Post = ({ post }) => {
+  return (
+    <div className="flex p-3 cursor-pointer border-b border-gray-200">
+      {/* user image */}
+      <img
+        src={post.userImg}
+        alt="user-img"
+        className="h-11 w-11 rounded-full mr-4"
+      />
+      {/* righr side */}
+      <div className="">
+        {/* header */}
+
+        <div className="flex items-center justify-between">
+          {/* post user info */}
+          {/* whitespace-nowrap เวลา zoom มันจะไม่ตกอีกบรรทัด */}
+          <div className="flex items-center space-x-1 whitespace-nowrap">
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+              {post.name}
+            </h4>
+            <span className="text-sm sm:text-[15px]">@{post.username} - </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+              {post.timestamp}
+            </span>
+          </div>
+
+          {/* dot icon */}
+          <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2" />
+        </div>
+
+        {/* post text */}
+        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">
+          {post.text}
+        </p>
+
+        {/* post image */}
+        <img src={post.img} alt="" className="rounded-2xl mr-2" />
+
+        {/* icons */}
+        <div className="flex justify-between text-gray-500 p-2 ">
+          <ChatIcon className="h-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <TrashIcon className="h-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          <HeartIcon className="h-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          <ShareIcon className="h-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <ChartBarIcon className="h-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
