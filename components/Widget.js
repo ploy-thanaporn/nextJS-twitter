ea398370d81480217a0a54ee8c89636f5f6c4346
ploy-thanaporn <tnpsp1501@gmail.com>
2022-07-01 16:33:29 +0700

    create random user section of the widget

--- components/Widget.js
+++ components/Widget.js
@@ -31,7 +31,7 @@ const Widget = ({ newsResults, randomUserResults }) => {
         </button>
       </div>
       <div className="sticky top-16 text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
-        <h4 className="font-bold text-xl px-4">Who follow</h4>
+        <h4 className="font-bold text-xl px-4">Who to follow</h4>
         {randomUserResults.slice(0, userNum).map((user) => (
           <div
             className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200"
