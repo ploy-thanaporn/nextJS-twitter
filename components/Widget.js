9d916c5207538d635108937f43d4dba9a94a0e18
ploy-thanaporn <tnpsp1501@gmail.com>
2022-07-01 14:54:38 +0700

    create the widget

--- components/Widget.js
+++ components/Widget.js
@@ -1,7 +1,20 @@
-import React from "react";
+import { SearchIcon } from "@heroicons/react/outline";
 
 const Widget = () => {
-  return <div>Widgets</div>;
+  return (
+    <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
+      <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
+        <div className="flex items-center p-3 rounded-full bg-red-300 relative">
+          <SearchIcon className="h-5 z-50 text-gray-500" />
+          <input
+            type="text"
+            placeholder="Search Twitter"
+            className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg bg-gray-100"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Widget;
--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         <Feed />
 
         {/* widgets */}
-        {/* <Widget /> */}
+        <Widget />
 
         {/* modal */}
         {/* <Modal /> */}
