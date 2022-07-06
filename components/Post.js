29290afffc32295e686e775395366576aa852b82
ploy-thanaporn <tnpsp1501@gmail.com>
2022-07-06 16:24:16 +0700

    delete func update

--- components/Post.js
+++ components/Post.js
@@ -55,7 +55,9 @@ const Post = ({ post }) => {
   async function deletePost() {
     if (window.confirm("Are you sure you want to delete this post?")) {
       deleteDoc(doc(db, "posts", post.id));
-      deleteObject(ref(storage, `posts/${post.id}/image`));
+      if (post.data().image) {
+        deleteObject(ref(storage, `posts/${post.id}/image`));
+      }
     }
   }
 
