dfdeb03e12c2b68a183778ecba59890739431971
ploy-thanaporn <tnpsp1501@gmail.com>
2022-07-06 16:21:15 +0700

    update upload photo

--- components/Input.js
+++ components/Input.js
@@ -39,7 +39,7 @@ const Input = () => {
 
     const imageRef = ref(storage, `posts/${docRef.id}/image`);
 
-    if (setSelectedFile) {
+    if (selectedFile) {
       await uploadString(imageRef, selectedFile, "data_url").then(async () => {
         const downloadURL = await getDownloadURL(imageRef);
         await updateDoc(doc(db, "posts", docRef.id), {
